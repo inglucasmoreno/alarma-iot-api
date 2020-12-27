f0074e1b5ae8c94b0f7963ec6af939367252d97f
Lucas Omar Moreno <ing.lucasmoreno@gmail.com>
2020-12-27 17:57:22 -0300

    MQTT - Correcciones

--- mqtt/mqttConnect.js
+++ mqtt/mqttConnect.js
@@ -3,7 +3,8 @@ const socketIO = require('socket.io');  // Debe ser la version 2.3.0
 const Alarma = require('../models/alarma.model');
 
 const mqtt = require('mqtt');
-const topic = 'estado_alarma';
+const topic_envio = 'estado_alarma_back';
+const topic_recepcion = 'estado_alarma_esp32';
 const socketName = 'estado_alarma';
 const client = mqtt.connect('mqtt://localhost:1883');
 
@@ -13,8 +14,8 @@ const mqttConnect = (server) => {
     client.on('connect', () => {
         console.log(chalk.blue('[MQTT]') + ' - Conexion con Broker MQTT -> ' + chalk.green('[Correcta]')); 
         // Se suscribe al topico - Estado Alarma
-        client.subscribe(topic, () => {
-            console.log(chalk.blue('[MQTT]') + ` - Suscripción al topico - ${topic} -> ` + chalk.green('[Correcta]'));
+        client.subscribe(topic_recepcion, () => {
+            console.log(chalk.blue('[MQTT]') + ` - Suscripción al topico - ${topic_recepcion} -> ` + chalk.green('[Correcta]'));
         })
     })
 
@@ -32,8 +33,23 @@ const mqttConnect = (server) => {
         
         // Se escucha el socket "estado_alarma"
         socket.on(socketName, estado => {
+            let mqttPayload;
             io.emit(socketName, estado);            // Se reenvia a los otros clientes - WebSocket
-            client.publish(topic, estado);          // Se envia el estado por el topico MQTT
+            
+            switch (estado) {
+                case 'Desactivada':
+                    mqttPayload = 'd';
+                    break;
+                case 'Activada':
+                    mqttPayload = 'a';
+                    break;
+                case 'Sonando':
+                    mqttPayload = 's';
+                    break;
+                default:
+                    break;
+            }
+            client.publish(topic_envio, mqttPayload);       // Se envia el estado por el topico MQTT
         })
 
     });
@@ -41,15 +57,44 @@ const mqttConnect = (server) => {
     // Se escucha el topico "estado_alarma"
     client.on('message', async ( topicoEntrada , message) => {
         
-        // Se conviente a String el mensaje recibido
-        const estado = String(message);
-        
-        // Se envia estado por websocket a todos los clientes
-        io.emit(socketName, estado);
+        let estado;
 
-        // Se almacena estado en la base de datos
-        const alarma = await Alarma.find();
-        await Alarma.findByIdAndUpdate(alarma[0]._id, { estado }, {new: true});
+        switch (String(message)) {
+            case 'd':
+                estado = 'Desactivada'
+                break;
+            case 'a':
+                estado = 'Activada'
+                break;
+            case 's':
+                estado = 'Sonando'
+                break;  
+            case 'r':
+                let mqttPayload;
+                const alarma = await Alarma.find();
+                if(alarma[0].estado === 'Desactivada') mqttPayload = 'd';
+                else if(alarma[0].estado === 'Activada') mqttPayload = 'a';
+                else if(alarma[0].estado === 'Sonando') mqttPayload = 's';
+                console.log(mqttPayload);
+                client.publish(topic_envio, mqttPayload);
+                break;  
+            default:
+                break;
+        }
+
+        if(String(message) !== 'r'){
+            
+            console.log('Guardando nuevo estado');
+            console.log(estado);
+            
+            // Se envia estado por websocket a todos los clientes
+            io.emit(socketName, estado);
+            
+            // Se almacena estado en la base de datos
+            const alarma = await Alarma.find();
+            await Alarma.findByIdAndUpdate(alarma[0]._id, { estado }, {new: true});
+        }
+
     });
     
 }
